d32aefd5424ba074851860c0a26360c254abe20d
Dominik Riedig <me@dominikriedig.de>
2020-04-13 11:17:24 +0200

    Add patch st-font2-20190416-ba72500.diff

--- config.def.h
+++ config.def.h
@@ -5,7 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "monospace:size=12";
+/* Spare fonts */
+static char *font2[] = {
+  "JoyPixels:pixelsize=14:antialias=true:autohint=true"
+};
+
 static int borderpx = 2;
 
 /*
