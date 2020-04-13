7bfd7815e38199ef52cdeb1a1595c76a442207a4
Dominik Riedig <me@dominikriedig.de>
2020-04-13 12:13:05 +0200

    Change appearance with custom color scheme, cursor changes and alpha settings

--- config.def.h
+++ config.def.h
@@ -88,37 +88,36 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;           //< alpha value used when the window is focused.
-float alphaUnfocussed = 0.6; //< alpha value used when the focus is lost
+float alpha = 0.95;  //< alpha value used when the window is focused.
+float alphaUnfocussed = 0.90;  //< alpha value used when the focus is lost
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#000000",  /* Black */
+	"#cc0000",  /* Red */
+	"#4e9a06",  /* Green */
+	"#c4a000",  /* Yellow */
+	"#3465a4",  /* Blue */
+	"#75507b",  /* Purple */
+	"#06989a",  /* Cyan */
+	"#d3d7cf",  /* White */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#555753",  /* Gray */
+	"#ef2929",  /* Red */
+	"#8ae234",  /* Green */
+	"#fce94f",  /* Yellow */
+	"#729fcf",  /* Blue */
+	"#ad7fa8",  /* Purple */
+	"#34e2e2",  /* Cyan */
+	"#eeeeec",  /* White */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+	"#eeeeec",  /* defaultfg, defaultcs */
+	"#2e203a"   /* defaultbg, defaultrcs */
 };
 
 
@@ -126,8 +125,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
@@ -138,7 +137,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
